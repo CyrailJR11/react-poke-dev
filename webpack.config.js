49db0107d1c3c42064915b31a73ee018d6586679
CustomNico <nicu17.1995@gmail.com>
2026-02-06 10:02:01 +0100

    Refactor webpack.config.js for production mode
    Updated webpack configuration for production deployment.

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./src/index.html",
-    filename: "./index.html"
-});
+
 module.exports = {
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: { presets: ['@babel/preset-react'] },
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i, 
-                loader: 'file-loader',
-                options: {
-                  name: 'src/assets/[name].[ext]'
-                }
-            }
-        ]
-    },
-    plugins: [htmlPlugin],
-    output: {
-        publicPath: '/'
-    },
-    devServer: {
-        historyApiFallback: true
-    }
+  mode: "production", // IMPORTANT for Vercel
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-react"] }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "file-loader",
+        options: {
+          name: "assets/[name].[ext]"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  }
 };
